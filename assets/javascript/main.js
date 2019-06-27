3e1050877d73364cb3c3fec1ec70a34c78cd2c69
Christina Truong <christinaqtruong@gmail.com>
2019-06-27 11:11:34 -0500

    Created input form and reorganized code so all the functions are listed prior to being called back

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -168,6 +168,62 @@ var lives = randomPokemon.length + 10;
 //dashes holds all the blanks joined together by spaces
 var dashes;
 
+//function that prints the instructions on the screen
+var instructions = function (target, message, index, interval){   
+  if (index < message.length) {
+    $(target).append(message[index++]);
+    
+    setTimeout(function () { instructions(target, message, index, interval); }, interval);
+  
+  }
+}
+
+//function that enables the Start Button, shows professor oak, and prints the instructions
+function enableStartBtn() {
+  document.getElementById('start-btn').removeAttribute('disabled');
+
+  $(function () {
+  instructions("#instructions", "WAIT! It's dangerous out there! Take one of these Pokémon! If you can guess its name, that is.", 0, 80);   
+  });
+}
+
+//function that shows professor oak
+function professorOak(){
+  $('#professor-oak').removeAttr('style'); 
+}
+
+//function that plays Hangman
+function play(){
+  //hides professor oak
+  document.getElementById('professor-oak').setAttribute('style', 'display:none;');
+
+  //hides instructions
+
+  //populates blanks array with dashes equal to the random pokemon length
+  for(var i=0; i < randomPokemon.length; i++){
+    blanks[i] = "_";
+  }
+
+  //joins the dashes into a string with spaces in between
+  dashes = blanks.join(" ");
+
+  //displays the dashes on the screen
+  $('#dashes-display').text(dashes);
+
+  //creates an input form on the page
+  var inputField = $('<input id="input-field" type="text">');
+
+  $('#input-form').append(inputField);
+}
+
+//push start button to start game
+$('#start-btn').on('click', function(){
+
+  //initiate Pokemon Hangman
+  play();
+
+})
+
 //push on button to see game instructions and activate play button
 $('#on-btn').on('click', function(){
 
@@ -178,47 +234,3 @@ $('#on-btn').on('click', function(){
     professorOak();
 
 })
-
-function enableStartBtn() {
-  document.getElementById('start-btn').removeAttribute('disabled');
-}
-
-function professorOak(){
-  $('#professor-oak').removeAttr('style');
-
-  // $(document).ready(function(){
-  //   var instructions = function(target, message, index, interval){
-  //     if(index < message.length){
-  //       $(target).append(message[index++]);
-  
-  //       setTimeout(function(){instructions(target, message, index, interval);}, interval);
-  //     }
-  //   }
-  // })
-
-  // $(function(){
-  //   instructions('#instructions', "WAIT! It's dangerous out there! Take one of these Pokémon! If you can guess its name, that is.", 0, 80);
-  // })
-}
-
-//push start button to start game
-$('#start-btn').on('click', function(){
-
-    //start one round
-    play();
-
-})
-
-function play(){
-  console.log("The start button is ON!")
-
-  document.getElementById('professor-oak').setAttribute('style', 'display:none;');
-
-  for(var i=0; i < randomPokemon.length; i++){
-    blanks[i] = "_";
-  }
-  dashes = blanks.join(" ");
-
-  //where dashes will be displayed
-  $('#dashes-display').text(dashes);
-}
